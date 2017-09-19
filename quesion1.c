917c9467bbf05c2f07bfebb1b54d5096cb85a107
Emily Rother <emilyrother1@EmilysMac.local>
2017-09-19 11:30:46 -0400

    fizz buzz first commit

---
+++ quesion1.c
@@ -0,0 +1,20 @@
+//fizz buzz oracle prep
+
+#include <malloc.h>
+#include <string.h>
+
+string[] fizzbuzz(int n){
+	string[n] answer;
+	for(int i=0; i<n; i++){
+		if(i%3==0){
+			answer[i]="Fizz";
+		}
+		else if(i%5==0){
+			answer[i]="Buzz";
+		}
+		else{
+			answer[i] = i;
+		}
+	}
+	return answer;
+}
